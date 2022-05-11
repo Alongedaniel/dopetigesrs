37e5abf40f56c369417ac5381d603cbcd7f80751
Alongedaniel <alongedaniel41@gmail.com>
2022-05-11 15:20:01 +0100

    Updated menubar

--- script/script.js
+++ script/script.js
@@ -1,8 +1,17 @@
 let menuToggler = document.querySelector(".menu-bar");
 let menu = document.querySelector(".mobile-nav");
+let close = document.querySelectorAll(".close");
 
 function toggleMenu() {
   menuToggler.addEventListener("click", () => {
-    menu.classList.toggle('opacity')
+    menu.classList.add("opacity");
+  });
+}
+
+function closeMenu() {
+  close.forEach(function (closer) {
+    closer.addEventListener("click", () => {
+      menu.classList.remove("opacity");
+    });
   });
 }
