a0b5a81c791bc16ce65b0005c29c53971e97b868
Alongedaniel <alongedaniel41@gmail.com>
2022-05-13 22:29:35 +0100

    Update and modifications

--- script/script.js
+++ script/script.js
@@ -1,17 +1,26 @@
 let menuToggler = document.querySelector(".menu-bar");
 let menu = document.querySelector(".mobile-nav");
 let close = document.querySelectorAll(".close");
+let sampleImage = document.querySelector(".sample-image");
+let openImage = document.querySelector(".open-image");
+let closeImage = document.querySelector(".close-img");
 
 function toggleMenu() {
-  menuToggler.addEventListener("click", () => {
-    menu.classList.add("opacity");
-  });
+  if (menu.classList.add("opacity") === false) {
+    menuToggler.addEventListener("click", () => {
+      menu.classList.add("opacity");
+    });
+  }
 }
 
 function closeMenu() {
-  close.forEach(function (closer) {
-    closer.addEventListener("click", () => {
-      menu.classList.remove("opacity");
-    });
-  });
+  menu.classList.remove("opacity");
+}
+
+function sampleImg() {
+  openImage.classList.toggle("see");
+}
+
+function closeImg() {
+  openImage.classList.remove("see");
 }
